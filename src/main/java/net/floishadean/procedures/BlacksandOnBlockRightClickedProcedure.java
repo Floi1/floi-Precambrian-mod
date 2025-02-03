853994fa18b23d01ca2abb50bce1e4b712521719
Floi1 <hamnsar@gmail.com>
2025-02-03 15:29:10 +0000

    fixes

---
+++ src/main/java/net/floishadean/procedures/BlacksandOnBlockRightClickedProcedure.java
@@ -0,0 +1,57 @@
+package net.floishadean.procedures;
+
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.BlockPos;
+
+import net.floishadean.init.FloisHadeanModModBlocks;
+
+public class BlacksandOnBlockRightClickedProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() instanceof HoeItem && FloisHadeanModModBlocks.BLACKSAND.get() == (world.getBlockState(BlockPos.containing(x, y, z))).getBlock()) {
+			if (world instanceof ServerLevel _level) {
+				(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).hurtAndBreak(1, _level, null, _stkprov -> {
+				});
+			}
+			{
+				BlockPos _bp = BlockPos.containing(x, y, z);
+				BlockState _bs = FloisHadeanModModBlocks.RIPPLEDBLACKSAND.get().defaultBlockState();
+				BlockState _bso = world.getBlockState(_bp);
+				for (Property<?> _propertyOld : _bso.getProperties()) {
+					Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
+					if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
+						try {
+							_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
+						} catch (Exception e) {
+						}
+				}
+				BlockEntity _be = world.getBlockEntity(_bp);
+				CompoundTag _bnbt = null;
+				if (_be != null) {
+					_bnbt = _be.saveWithFullMetadata(world.registryAccess());
+					_be.setRemoved();
+				}
+				world.setBlock(_bp, _bs, 3);
+				if (_bnbt != null) {
+					_be = world.getBlockEntity(_bp);
+					if (_be != null) {
+						try {
+							_be.loadWithComponents(_bnbt, world.registryAccess());
+						} catch (Exception ignored) {
+						}
+					}
+				}
+			}
+		}
+	}
+}
